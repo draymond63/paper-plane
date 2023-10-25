6c7dd941e075f3857c9558ba91518242629dee90
Daniel Raymond <dan@raymond.ch>
2023-10-25 17:03:13 -0400

    Clean up, removed COM position

--- airplane_sim.py
+++ airplane_sim.py
@@ -18,10 +18,11 @@ def equations_of_motion(t, y, err_limit=100000):
     wing_area = 0.01  # wing area, m^2
     mass = 0.01  # mass, kg
     inertia = 0.0001  # mass moment of inertia, kg*m^2
-    COM = as_vector(0.05, 0)     # position of center of mass from bottom of tail edge, m (x, y) 
-    COP = as_vector(0.1, 0)  # position of center of pressure from bottom of tail edge, m (x, y)
-    critical_angle = np.deg2rad(45)  # critical angle of attack, radians
-    cop_arm_plane = COP - COM # COP arm position relative to the plane
+    cop_plane = as_vector(0.05, 0)  # position of center of pressure relative to the COM, relative to the plane, m
+    critical_angle = np.deg2rad(90)  # critical angle of attack, radians
+
+    cop_angle_plane = np.arctan2(*cop_plane)
+    Fg = as_vector(0, mass * -g) # Force of gravity
 
     vx, vy, alpha, dalpha_dt = y[2:]
     speed = np.sqrt(vx**2 + vy**2)
@@ -29,41 +30,38 @@ def equations_of_motion(t, y, err_limit=100000):
     attack_angle = alpha - motion_angle # Angle of attack w.r.t to the direction of motion
 
     # Lift and Drag calculations
-    q = 0.5 * rho * speed**2  # dynamic pressure
-    CL = CL_alpha * np.sin(attack_angle)  # Lift coefficient
+    q = 0.5 * rho * speed**2 # Dynamic pressure
+    CL = CL_alpha * np.sin(attack_angle) # Lift coefficient
     planform_area = wing_area * np.cos(attack_angle) # Projected wing area w.r.t to the direction of motion
     frontal_area = wing_area * np.sin(attack_angle) # Projected wing area w.r.t to the direction of motion
 
     # Limit max angle of attack. Too big, and the flow of air over the top of the wing will no longer be smooth and the lift suddenly decreases
     if np.abs(attack_angle) > critical_angle:
         lift = 0
-        # CL = - 0.1
     else:
         lift = q * planform_area * CL
     # TODO: Lift should be negative when attack angle is negative
     lift_vec = as_vector(magnitude=lift, angle=np.pi/2 - motion_angle) # Lift is perpendicular to the direction of motion
 
     # Aerodynamic forces
-    CD = 0.01 + CL**2 / (np.pi * 4)
-    drag = q * frontal_area * CD # TODO: Include friction drag?
+    CD = 0.01 + CL**2 / (np.pi * 4) # Drag coefficient
+    drag = q * frontal_area * CD
     drag_vec = as_vector(magnitude=-drag, angle=motion_angle)
     pressure_forces = lift_vec + drag_vec
-    # Total translational forces
-    F = pressure_forces + as_vector(0, mass * -g)
-    dvx_dt = F[0] / mass
-    dvy_dt = F[1] / mass
+    # Total translational forces, in x-y reference frame
+    F = pressure_forces + Fg
+    dv_dt = F / mass
 
     # Torque and angular acceleration
-    pressure_angle = alpha + np.arctan2(cop_arm_plane[0], cop_arm_plane[1])
-    cop_arm = as_vector(magnitude=np.linalg.norm(cop_arm_plane), angle=pressure_angle)
+    cop_angle = alpha + cop_angle_plane
+    cop_arm = as_vector(magnitude=np.linalg.norm(cop_plane), angle=cop_angle) # COP arm position relative to the x-y reference frame
     tau = np.cross(cop_arm, pressure_forces) # Cross product of lift vector and distance from center of mass
-    # TODO: Should depend on the direction of the angular velocity
-    rotational_drag = 0.5 * rho * dalpha_dt**2 * wing_area * (COP[0] - COM[0]) * CD * np.sign(dalpha_dt)
-    # rotational_drag = 0.5 * rho * dalpha_dt**2 * wing_area * np.linalg.norm(COP - COM) * CD * np.sign(dalpha_dt)
+    rotational_drag = 0.5 * rho * dalpha_dt**2 * wing_area * np.linalg.norm(cop_arm) * CD * np.sign(dalpha_dt)
     tau -= rotational_drag
     domega_dt = tau / inertia
 
-    dstate_dt = [vx, vy, dvx_dt, dvy_dt, dalpha_dt, domega_dt]
+    dstate_dt = [vx, vy, dv_dt[0], dv_dt[1], dalpha_dt, domega_dt]
+    print(dstate_dt)
     if np.any(np.asarray(dstate_dt) > err_limit):
         raise RuntimeError(f"Things accelerated out of control. Reached {dstate_dt}")
     return dstate_dt
@@ -79,7 +77,6 @@ if __name__ == "__main__":
 
     duration = 5
     t = np.linspace(0, duration, 1001)
-    # TODO: Plane stalls out and then the angle never recovers. Why?
     solution = solve_ivp(equations_of_motion, (0, duration), initial_conditions, t_eval=t)
 
     x, y, vx, vy, alpha, omega = solution.y
