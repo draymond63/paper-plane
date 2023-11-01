44409568b4ab7e2ed4769153c177cab17f58db29
Daniel Raymond <dan@raymond.ch>
2023-11-01 18:37:18 -0400

    Cleaned up plotting

--- airplane_sim.py
+++ airplane_sim.py
@@ -51,7 +51,7 @@ class PlaneSim:
         motion_angle = np.arctan2(vy, vx) # Angle of motion w.r.t to the x-axis
         attack_angle = alpha - motion_angle # Angle of attack w.r.t to the direction of motion
         # Lift and Drag calculations
-        Fp = self.calc_pressure_forces(speed, motion_angle, attack_angle)
+        Fp = np.sum(self.calc_pressure_forces(speed, motion_angle, attack_angle), axis=0)
         dv_dt = (Fp + self.Fg) / self.plane.mass
         # Torque and angular acceleration
         tau = self.calc_torque(alpha, dalpha_dt, attack_angle, Fp)
@@ -62,16 +62,19 @@ class PlaneSim:
         #     raise RuntimeError(f"Things accelerated out of control. Reached {dstate_dt}")
         return dstate_dt
 
+    def dynamic_pressure(self, speed):
+        return 0.5 * self.rho * speed**2
+
     def calc_pressure_forces(self, speed, motion_angle, attack_angle):
-        q = 0.5 * self.rho * speed**2 # Dynamic pressure
+        q = self.dynamic_pressure(speed) # Dynamic pressure
         planform_area = self.plane.wing_area * np.cos(attack_angle) # Projected wing area w.r.t to the direction of motion
         frontal_area = self.plane.wing_area * np.sin(attack_angle) # Projected wing area w.r.t to the direction of motion
         # Limit max angle of attack. Too big, and the flow of air over the top of the wing will no longer be smooth and the lift suddenly decreases
-        lift = q * planform_area * self.get_CL(attack_angle) if np.abs(attack_angle) <= self.critical_angle else 0
+        lift = q * planform_area * self.get_CL(attack_angle) # if np.abs(attack_angle) <= self.critical_angle else 0
         lift_vec = as_vector(magnitude=lift, angle=np.pi/2 - motion_angle) # Lift is perpendicular to the direction of motion
         drag = q * frontal_area * self.get_CD(attack_angle)
         drag_vec = as_vector(magnitude=-drag, angle=motion_angle)
-        return np.sum([lift_vec, drag_vec], axis=0) # Type-checking doesn't like using + operator for some reason
+        return lift_vec, drag_vec
 
     def calc_torque(self, alpha, dalpha_dt, attack_angle, Fp):
         cop_arm = as_vector(magnitude=self.cop_arm_length, angle=alpha + self.cop_angle_plane) # COP arm position relative to the x-y reference frame
@@ -79,14 +82,17 @@ class PlaneSim:
         tau -= self.calc_rotational_drag(dalpha_dt, attack_angle)
         return tau
 
+    # Spin-damping moment
     def calc_rotational_drag(self, dalpha_dt, attack_angle):
-        return 0.5 * self.rho * dalpha_dt**2 * self.plane.wing_area * self.cop_arm_length * self.get_CD(attack_angle) * np.sign(dalpha_dt)
+        # TODO: Should dynamic pressure be a function of speed, not dalpha_dt?
+        return np.sign(dalpha_dt) * self.dynamic_pressure(dalpha_dt) * self.plane.wing_area * self.cop_arm_length * self.get_CD(attack_angle)
 
     def run(self, t):
         launch_angle = 0
-        init_velocity = as_vector(magnitude=5, angle=np.deg2rad(launch_angle))
+        init_velocity = as_vector(magnitude=0, angle=np.deg2rad(launch_angle))
         init_attack_angle = 0 # degrees
         init_height = 2 # meters
+        # Initial conditions: [x_position, y_position, x_velocity, y_velocity, angle_of_attack, angular_velocity]
         initial_conditions = [0, init_height, init_velocity[0], init_velocity[1], np.deg2rad(init_attack_angle + launch_angle), 0]
 
         duration = [*t][-1]
@@ -102,31 +108,33 @@ class PlaneSim:
 
         return solution.y
 
+    def plot(self, results, with_forces=False, with_angle=True):
+        x, y, vx, vy, alpha, omega = results
+        above_ground = y >= 0
+        plt.plot(x[above_ground], y[above_ground])
+        legend = ['Flight path']
+        if with_angle:
+            direction_vecs = as_vector(magnitude=1, angle=alpha[above_ground])
+            plt.quiver(x[above_ground], y[above_ground], direction_vecs[0], direction_vecs[1], headwidth=1, scale=50)
+            legend.append('Angle of attack')
+        if with_forces:
+            speed = np.sqrt(vx**2 + vy**2)
+            motion_angle = np.arctan2(vy, vx)
+            attack_angle = alpha - motion_angle
+            lift, drag = self.calc_pressure_forces(speed, motion_angle, attack_angle)
+            plt.quiver(x[above_ground], y[above_ground], lift[0][above_ground], lift[1][above_ground], headwidth=1, scale=2 , color='r')
+            plt.quiver(x[above_ground], y[above_ground], drag[0][above_ground], drag[1][above_ground], headwidth=1, scale=2 , color='b')
+            legend.extend(['Lift', 'Drag'])
+        plt.title('Paper Airplane Flight')
+        plt.xlabel('Distance (m)')
+        plt.ylabel('Height (m)')
+        plt.legend(legend)
+        plt.grid(True)
+        plt.show()
 
 
 if __name__ == "__main__":
     duration = 2
     t = np.linspace(0, duration, 201)
-    x, y, vx, vy, alpha, omega = PlaneSim().run(t)
-    motion_angle = np.arctan2(vy, vx)
-    above_ground = np.where(y >= 0)[0]
-
-    # Plotting
-    plt.figure()
-    plt.plot(x[above_ground], y[above_ground])
-    # plt.plot(x[above_ground], alpha[above_ground])
-    # plt.plot(t, x)
-    # plt.plot(t, y)
-    # plt.plot(x[above_ground], motion_angle[above_ground] - alpha[above_ground])
-    # print("\n".join([f"{t[i]:.2f}: {string_vec(forces[i])}" for i in range(len(t))]))
-    arrow_size = 1/5
-    for i in range(0, len(x[above_ground]), 10):
-        direction_vec = as_vector(magnitude=arrow_size, angle=alpha[i])
-        plt.arrow(x[i], y[i], direction_vec[0], direction_vec[1], width=0.0001)
-        
-    plt.title('Paper Airplane Flight')
-    plt.xlabel('Distance (m)')
-    plt.ylabel('Height (m)')
-    plt.grid(True)
-    plt.show()
-    # Initial conditions: [x_position, y_position, x_velocity, y_velocity, angle_of_attack, angular_velocity]
+    sim = PlaneSim()
+    sim.plot(sim.run(t), with_forces=True)
